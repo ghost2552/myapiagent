ffb5ba0c6f19a349206692fcc9479c914a356e63
ghost2552 <ayron444222@gmail.com>
2025-09-22 23:31:27 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { google } from 'googleapis';
+const express = require('express');
+const bodyParser = require('body-parser');
+const { google } = require('googleapis');
 
 const app = express();
 const port = process.env.PORT || 10000;
